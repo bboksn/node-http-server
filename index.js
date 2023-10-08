9c81f4df94f2ab750d78a310d5f76989839ae126
bboks <bboksner@gmail.com>
2023-10-08 01:25:33 -0400

    index.js working

--- index.js
+++ index.js
@@ -2,8 +2,17 @@ const http = require('http');
 const url = require('url');
 const fs = require('fs');
 http.createServer((req, resp) => {
-let q = url.parse(req.url,true)
-page= "." + q.pathname;
-
-})
+    let q = url.parse(req.url, true)
+    page = q.pathname==="/" ? "./index.html":"." + q.pathname+".html";
+    fs.readFile(page,(err,data)=>{
+        if(err){
+            console.log(err)
+            resp.writeHead(404,{'Content-Type':'text/html'});
+            return resp.end("404 error")
+        }
+        resp.writeHead(200,{'Content-Type':'text/html'})
+        resp.write(data)
+        return resp.end();
+    })
+}).listen(8080);
 console.log('server started running port 8080')
